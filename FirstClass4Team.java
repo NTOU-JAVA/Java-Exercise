11b701d0f0961eaf6429478657f950937ed370a2
HPJ <pinjie0106@gmail.com>
2026-04-22 14:30:47 +0800

    新增我自己

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,6 @@ public class FirstClass4Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01357023\"\n姓名：\"林昱安\"");
         System.out.println("\"學號：\"01357028\"\n姓名：\"林彥宇\"");
+        System.out.println("\"學號：\"01357026\"\n姓名：\"黃品傑\"");
     }
 }
