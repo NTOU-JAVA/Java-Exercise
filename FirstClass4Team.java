95305819a68dadf7dd72d6a579eb6b366e192983
CuteLime <peterabc219@gmail.com>
2026-04-22 14:29:56 +0800

    新增資訊

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,5 +2,6 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01357023\"\n姓名：\"林昱安\"");
+        System.out.println("\"學號：\"01357028\"\n姓名：\"林彥宇\"");
     }
 }
